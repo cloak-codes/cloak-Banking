07830d69680898189f7c2a3400854d42c91ce125
geo <106122043+cloak-codes@users.noreply.github.com>
2023-03-21 16:10:16 -0700

    Update Billing.jsx

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -22,8 +22,9 @@ const Billing = () => (
         invoicing
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean
-        neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+        Easy, fast, secure.
+        <br> 
+        So you can get back to what's important in life.
       </p>
 
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
