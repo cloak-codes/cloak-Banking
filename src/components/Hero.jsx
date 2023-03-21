43a4456cdac3731092aed607bd81de9347fb316d
geo <106122043+cloak-codes@users.noreply.github.com>
2023-03-21 14:47:57 -0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,8 +34,7 @@ const Hero = () => {
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Our team of experts uses a methodology to identify the credit cards
-          most likely to fit your needs. We examine annual percentage rates,
-          annual fees.
+          most likely to fit your needs. We examine annual percentage rates and fees.
         </p>
       </div>
 
